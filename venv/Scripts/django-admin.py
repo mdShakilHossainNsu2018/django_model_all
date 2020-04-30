ad4ee272b85dd66fff3c98a293341fe629ba9903
mdShakilHossainNsu20!8 <shakilnsu2018@gmail.com>
2020-04-30 15:16:41 +0600

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\shaki\PycharmProjects\django_model_all\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
